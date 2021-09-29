20d29be523f86635ccf3978eef3715a5e873842d
Rohan Sinha <sinharohan3@gmail.com>
2021-09-29 18:54:27 -0400

    Update code.py
    Changes to Edit Functionality

--- code/code.py
+++ code/code.py
@@ -193,12 +193,17 @@ def edit2(m):
         bot.reply_to(m, "The date is incorrect")
         return
     
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.row_width = 2
+    choices = ['Date','Category','Cost']
+    for c in choices:
+        markup.add(c)
     
     for record in data_edit:
         i_edit = i_edit + 1
         record = record.split(',')
         if info[0] == record[0][0:11] and info[1] == record[1]:
-            choice = bot.reply_to(m, "What do you want to update?")
+            choice = bot.reply_to(m, "What do you want to update?", reply_markup = markup)
             bot.register_next_step_handler(choice, edit3)
             break
 
@@ -208,7 +213,7 @@ def edit3(m):
     data_edit = getUserHistory(chat_id)
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
     markup.row_width = 2
-    for cat in CATEGORIES:
+    for cat in spend_categories:
         markup.add(cat)
     if(choice1 == 'Date'):
         new_date = bot.reply_to(m, "Please enter the new date (in dd-mmm-yyy format)")
@@ -236,9 +241,9 @@ def edit_date(m):
         return
     record = data_edit[i_edit].split(',')
     record[0] = new_date + record[0][11:len(record[0])]
-    data_edit[i_edit] = record[0] + ',' + record[1] + ',' + int(record[2]) + '.0'
+    data_edit[i_edit] = record[0] + ',' + record[1] + ',' + record[2]
     user_list[str(chat_id)] = data_edit
-    writeJson(user_list)
+    write_json(user_list)
     bot.reply_to(m, "Date is updated")
     
 def edit_cat(m):
@@ -248,28 +253,28 @@ def edit_cat(m):
     new_cat = m.text
     record = data_edit[i_edit].split(',')
     record[1] = new_cat
-    data_edit[i_edit] = record[0] + ',' + record[1] + ',' + int(record[2]) + '.0'
+    data_edit[i_edit] = record[0] + ',' + record[1] + ',' + record[2]
     user_list[str(chat_id)] = data_edit
-    writeJson(user_list)
+    write_json(user_list)
     bot.reply_to(m, "Category is updated")
 
 def edit_cost(m):
     global i_edit
-    print(i_edit)
     new_cost = m.text
     chat_id = m.chat.id
     data_edit = getUserHistory(chat_id)
-    if(validateAmount(str(new_cost))):
-        record = data_edit[i_edit].split(',')                
-        record[2] = new_cost
-        data_edit[i_edit] = record[0] + ',' + record[1] + ',' + str(int(record[2])) +'.0'
-        user_list[str(chat_id)] = data_edit
-        writeJson(user_list)
-        bot.reply_to(m, "Cost is updated")
     
-    else:   
-        bot.reply_to(m, "The cost is invalid")
-        return
+    #if(validate_entered_amount(int(new_cost))== True):
+    record = data_edit[i_edit].split(',')                
+    record[2] = str(new_cost)+'.0'
+    data_edit[i_edit] = record[0] + ',' + record[1] + ',' + record[2]
+    user_list[str(chat_id)] = data_edit
+    write_json(user_list)
+    bot.reply_to(m, "Cost is updated")
+    
+    #else:
+     #   bot.reply_to(m, "The cost is invalid")
+      #  return
 
 #function to display total expenditure
 @bot.message_handler(commands=['display'])
