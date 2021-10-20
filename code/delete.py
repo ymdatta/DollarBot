2f129ddeb62361a1e5341c17acd3ac707ba8e19b
Athithya <sri.athithya.kruth@gmail.com>
2021-10-19 22:04:53 -0400

    fix: delete.py now works correctly

--- code/delete.py
+++ code/delete.py
@@ -4,9 +4,8 @@ import helper
 def run(message, bot):
     global user_list
     chat_id = message.chat.id
-    helper.read_json()
     delete_history_text = ""
-    user_list = helper.getUserHistory()
+    user_list = helper.read_json()
     if (str(chat_id) in user_list):
         helper.write_json(deleteHistory(chat_id))
         delete_history_text = "History has been deleted!"
