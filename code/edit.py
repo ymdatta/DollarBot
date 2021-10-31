a1bb0d076d2780586353ffd6df9a5cefa4474c6f
zedunaid <zunaid.s@somaiya.edu>
2021-10-30 21:47:17 -0400

    Adding test cases for eedit functionality

--- code/edit.py
+++ code/edit.py
@@ -9,7 +9,7 @@ def run(m, bot):
 
     if str(chat_id) in user_list:
         info = bot.reply_to(m, "Please enter the date and category of the transaction you made (Eg: 01-Mar-2021,Transport)")
-        bot.register_next_step_handler(info, edit2, bot, user_list)
+        bot.register_next_step_handler(info, select_category_to_be_updated, bot)
     else:
         bot.reply_to(chat_id, "No data found")
 
@@ -17,15 +17,14 @@ def run(m, bot):
 i_edit = -1
 
 
-def edit2(m, bot, user_list):
-    print("Inside edit 2")
+def select_category_to_be_updated(m, bot):
     global i_edit
     i_edit = -1
     chat_id = m.chat.id
     data_edit = helper.getUserHistory(chat_id)
     info = m.text
     date_format = "^(([0][1-9])|([1-2][0-9])|([3][0-1]))\-(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\-\d{4}$"
-    info = info.split(',')
+    info = [""] if info is None else info.split(',')
     x = re.search(date_format, info[0])
     if x is None:
         bot.reply_to(m, "The date is incorrect")
@@ -33,21 +32,19 @@ def edit2(m, bot, user_list):
 
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
     markup.row_width = 2
-    choices = ['Date', 'Category', 'Cost']
-    for c in choices:
+
+    for c in helper.getChoices():
         markup.add(c)
-    print("Outside record:")
     for record in data_edit:
         i_edit = i_edit + 1
         record = record.split(',')
         if info[0] == record[0][0:11] and str(info[1]).strip() == str(record[1]).strip():
-            print("Inside what do u want to update")
             choice = bot.reply_to(m, "What do you want to update?", reply_markup=markup)
-            bot.register_next_step_handler(choice, edit3, bot, user_list)
+            bot.register_next_step_handler(choice, enter_updated_data, bot)
             break
 
 
-def edit3(m, bot, user_list):
+def enter_updated_data(m, bot):
     choice1 = m.text
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
     markup.row_width = 2
@@ -56,19 +53,20 @@ def edit3(m, bot, user_list):
 
     if choice1 == 'Date':
         new_date = bot.reply_to(m, "Please enter the new date (in dd-mmm-yyy format)")
-        bot.register_next_step_handler(new_date, edit_date, bot, user_list)
+        bot.register_next_step_handler(new_date, edit_date, bot)
 
     if choice1 == 'Category':
         new_cat = bot.reply_to(m, "Please select the new category", reply_markup=markup)
-        bot.register_next_step_handler(new_cat, edit_cat, bot, user_list)
+        bot.register_next_step_handler(new_cat, edit_cat, bot)
 
     if choice1 == 'Cost':
         new_cost = bot.reply_to(m, "Please type the new cost")
-        bot.register_next_step_handler(new_cost, edit_cost, bot, user_list)
+        bot.register_next_step_handler(new_cost, edit_cost, bot)
 
 
 def edit_date(m, bot, user_list):
     global i_edit
+    user_list = helper.read_json()
     new_date = m.text
     date_format = "^(([0][1-9])|([1-2][0-9])|([3][0-1]))\-(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\-\d{4}$"
     chat_id = m.chat.id
@@ -85,8 +83,9 @@ def edit_date(m, bot, user_list):
     bot.reply_to(m, "Date is updated")
 
 
-def edit_cat(m, bot, user_list):
+def edit_cat(m, bot):
     global i_edit
+    user_list = helper.read_json()
     chat_id = m.chat.id
     data_edit = helper.getUserHistory(chat_id)
     new_cat = m.text
@@ -98,8 +97,9 @@ def edit_cat(m, bot, user_list):
     bot.reply_to(m, "Category is updated")
 
 
-def edit_cost(m, bot, user_list):
+def edit_cost(m, bot):
     global i_edit
+    user_list = helper.read_json()
     new_cost = m.text
     chat_id = m.chat.id
     data_edit = helper.getUserHistory(chat_id)
