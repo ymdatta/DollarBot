b54aaad59b7ea02a1066b68f45dda5855e9ede92
Athithya <sri.athithya.kruth@gmail.com>
2021-10-31 22:44:51 -0400

    fixed tests in test_add.py

--- test/test_add.py
+++ test/test_add.py
@@ -3,6 +3,12 @@ import json
 from mock.mock import patch
 from telebot import types
 from code import add
+from mock import ANY
+
+
+dateFormat = '%d-%b-%Y'
+timeFormat = '%H:%M'
+monthFormat = '%b-%Y'
 
 
 @patch('telebot.telebot')
@@ -52,12 +58,17 @@ def test_post_amount_input_working(mock_telebot, mocker):
 def test_post_amount_input_working_withdata(mock_telebot, mocker):
     mc = mock_telebot.return_value
     mc.send_message.return_value = True
-    mocker.patch.object(add, 'helper')
+    mocker.patch.object(add, 'helper')    
     add.helper.validate_entered_amount.return_value = 10
     add.helper.write_json.return_value = True
+    add.helper.getDateFormat.return_value = dateFormat
+    add.helper.getTimeFormat.return_value = timeFormat
+
+    mocker.patch.object(add, 'option')
+    add.option.return_value = {11, "here"}
 
     message = create_message("hello from testing!")
-    add.post_category_selection(message, mc)
+    add.post_amount_input(message, mc)
     assert(mc.send_message.called)
 
 
@@ -68,10 +79,31 @@ def test_post_amount_input_nonworking(mock_telebot, mocker):
     mc.reply_to.return_value = True
     mocker.patch.object(add, 'helper')
     add.helper.validate_entered_amount.return_value = 0
+    message = create_message("hello from testing!")
+    add.post_amount_input(message, mc)
+    assert(mc.reply_to.called)
+
+
+@patch('telebot.telebot')
+def test_post_amount_input_working_withdata_chatid(mock_telebot, mocker):
+    mc = mock_telebot.return_value
+    mc.send_message.return_value = True
+    mocker.patch.object(add, 'helper')    
+    add.helper.validate_entered_amount.return_value = 10
+    add.helper.write_json.return_value = True
+    add.helper.getDateFormat.return_value = dateFormat
+    add.helper.getTimeFormat.return_value = timeFormat
+
+    mocker.patch.object(add, 'option')
+    add.option = {11, "here"}
+    test_option = {}
+    test_option[11] = "here"
+    add.option = test_option
 
     message = create_message("hello from testing!")
-    add.post_category_selection(message, mc)
-    assert(mc.reply_to.called)
+    add.post_amount_input(message, mc)
+    assert(mc.send_message.called)
+    assert(mc.send_message.called_with(11, ANY))
 
 
 def test_add_user_record_nonworking(mocker):
