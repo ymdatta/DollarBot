c4ecdef27a0c1cb7bc494e1d39e5ac57e79dbe11
Athithya <sri.athithya.kruth@gmail.com>
2021-10-31 22:02:06 -0400

    added test in test_add for add record

--- test/test_add.py
+++ test/test_add.py
@@ -1,3 +1,5 @@
+import os
+import json
 from mock.mock import patch
 from telebot import types
 from code import add
@@ -72,16 +74,41 @@ def test_post_amount_input_nonworking(mock_telebot, mocker):
     assert(mc.reply_to.called)
 
 
-def test_add_user_record(mocker):
+def test_add_user_record_nonworking(mocker):
     mocker.patch.object(add, 'helper')
     add.helper.read_json.return_value = {}
 
     addeduserrecord = add.add_user_record(1, "record : test")
-    print(addeduserrecord)
     assert(addeduserrecord)
+
+
+def test_add_user_record_working(mocker):
+    MOCK_USER_DATA = test_read_json()
+    mocker.patch.object(add, 'helper')
+    add.helper.read_json.return_value = MOCK_USER_DATA
+    print(len(MOCK_USER_DATA))
+    addeduserrecord = add.add_user_record(1, "record : test")
+    print(len(MOCK_USER_DATA), " here")
+    if(len(MOCK_USER_DATA) + 1 == len(addeduserrecord)):
+        assert True
 
 
 def create_message(text):
     params = {'messagebody': text}
     chat = types.User(11, False, 'test')
     return types.Message(1, None, None, chat, 'text', params, "")
+
+
+def test_read_json():
+    try:
+        if not os.path.exists('./test/dummy_expense_record.json'):
+            with open('./test/dummy_expense_record.json', 'w') as json_file:
+                json_file.write('{}')
+            return json.dumps('{}')
+        elif os.stat('./test/dummy_expense_record.json').st_size != 0:
+            with open('./test/dummy_expense_record.json') as expense_record:
+                expense_record_data = json.load(expense_record)
+            return expense_record_data
+
+    except FileNotFoundError:
+        print("---------NO RECORDS FOUND---------")
