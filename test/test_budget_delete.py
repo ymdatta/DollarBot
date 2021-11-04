601eced6b5359df433826ea78c43d73e81f08fd0
Athithya <sri.athithya.kruth@gmail.com>
2021-11-03 20:30:01 -0400

    added test cases for budget_delete

---
+++ test/test_budget_delete.py
@@ -0,0 +1,25 @@
+from code import budget_delete
+from mock.mock import patch
+from telebot import types
+
+
+@patch('telebot.telebot')
+def test_run_normal_case(mock_telebot, mocker):
+    mc = mock_telebot.return_value
+    mc.send_message.return_value = True
+
+    mocker.patch.object(budget_delete, 'helper')
+    budget_delete.helper.read_json.return_value = {11}
+
+    message = create_message("hello from testing")
+    budget_delete.run(message, mc)
+
+    mc.send_message.assert_called_with(11, 'Budget deleted!')
+
+
+def create_message(text):
+    params = {'messagebody': text}
+    chat = types.User(11, False, 'test')
+    message = types.Message(1, None, None, chat, 'text', params, "")
+    message.text = text
+    return message
