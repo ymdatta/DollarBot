791188bac7b420f985e1394dcf8f8bd3b2c014b8
sreehith <ysreehith.rao@gmail.com>
2023-11-19 14:14:18 -0500

    added exception handling code in pdf.py

--- code/download_pdf.py
+++ code/download_pdf.py
@@ -2,6 +2,12 @@ import helper
 import logging
 from matplotlib import pyplot as plt
 
+# Constants
+TOP_MARGIN = 0.8
+LINE_HEIGHT = 0.15
+FONT_SIZE_TITLE = 20
+FONT_SIZE_TEXT = 14
+
 def generate_expense_history_plot(user_history):
     """
     generate_expense_history_plot(user_history): This function generates a Matplotlib plot of the user's expense history.
@@ -12,37 +18,46 @@ def generate_expense_history_plot(user_history):
     Returns:
         fig (Matplotlib.figure.Figure): Matplotlib figure object containing the expense history plot.
     """
-    fig, ax = plt.subplots()
-    top = 0.8
+    try:
+        fig, ax = plt.subplots()
+        top = TOP_MARGIN
 
-    if not user_history:
-        ax.text(
-            0.1,
-            top,
-            "No record found!",
-            horizontalalignment="left",
-            verticalalignment="center",
-            transform=ax.transAxes,
-            fontsize=20,
-        )
-    else:
-        for rec in user_history:
-            date, category, amount = rec.split(",")
-            rec_str = f"{amount}$ {category} expense on {date}"
+        if not user_history:
             ax.text(
-                0,
+                0.1,
                 top,
-                rec_str,
+                "No record found!",
                 horizontalalignment="left",
                 verticalalignment="center",
                 transform=ax.transAxes,
-                fontsize=14,
-                bbox=dict(facecolor="blue", alpha=0.1),
+                fontsize=FONT_SIZE_TITLE,
             )
-            top -= 0.15
+        else:
+            for rec in user_history:
+                try:
+                    date, category, amount = rec.split(",")
+                except ValueError as ve:
+                    raise ValueError(f"Error parsing user history data: {ve}")
 
-    ax.axis("off")
-    return fig
+                rec_str = f"{amount}$ {category} expense on {date}"
+                ax.text(
+                    0,
+                    top,
+                    rec_str,
+                    horizontalalignment="left",
+                    verticalalignment="center",
+                    transform=ax.transAxes,
+                    fontsize=FONT_SIZE_TEXT,
+                    bbox=dict(facecolor="blue", alpha=0.1),
+                )
+                top -= LINE_HEIGHT
+
+        ax.axis("off")
+        return fig
+
+    except Exception as e:
+        logging.exception(str(e))
+        raise
 
 def save_and_send_pdf(fig, chat_id, bot):
     """
@@ -56,12 +71,17 @@ def save_and_send_pdf(fig, chat_id, bot):
     Returns:
         None
     """
-    pdf_path = "expense_history.pdf"
-    fig.savefig(pdf_path)
-    plt.close(fig)
+    try:
+        pdf_path = "expense_history.pdf"
+        fig.savefig(pdf_path)
+        plt.close(fig)
 
-    bot.send_message(chat_id, "expense_history.pdf is created")
-    bot.send_document(chat_id, open(pdf_path, "rb"))
+        bot.send_message(chat_id, "expense_history.pdf is created")
+        bot.send_document(chat_id, open(pdf_path, "rb"))
+
+    except Exception as e:
+        logging.exception(str(e))
+        raise
 
 def run(message, bot):
     """
@@ -75,7 +95,8 @@ def run(message, bot):
         None
 
     Raises:
-        Exception: If an error occurs during the process.
+        ValueError: If there is an issue parsing the user history data.
+        Exception: If an unexpected error occurs during the process.
 
     This function generates a PDF file containing the user's expense history plot and sends it as a document using a Telegram bot.
     """
@@ -85,6 +106,10 @@ def run(message, bot):
         user_history = helper.getUserHistory(chat_id)
         fig = generate_expense_history_plot(user_history)
         save_and_send_pdf(fig, chat_id, bot)
+
+    except ValueError as ve:
+        bot.reply_to(message, f"Oops! {ve}")
+
     except Exception as e:
         logging.exception(str(e))
-        bot.reply_to(message, str(e))
+        bot.reply_to(message, "Oops! An unexpected error occurred.")
