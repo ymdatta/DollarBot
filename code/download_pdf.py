1101e4c6ba5ab926a1e1c5e5a3e80700cd606ad7
sreehith <ysreehith.rao@gmail.com>
2023-11-19 14:11:03 -0500

    added comments in pdf.py

--- code/download_pdf.py
+++ code/download_pdf.py
@@ -3,6 +3,15 @@ import logging
 from matplotlib import pyplot as plt
 
 def generate_expense_history_plot(user_history):
+    """
+    generate_expense_history_plot(user_history): This function generates a Matplotlib plot of the user's expense history.
+
+    Args:
+        user_history (list): List of strings representing user's expense history records.
+
+    Returns:
+        fig (Matplotlib.figure.Figure): Matplotlib figure object containing the expense history plot.
+    """
     fig, ax = plt.subplots()
     top = 0.8
 
@@ -36,7 +45,17 @@ def generate_expense_history_plot(user_history):
     return fig
 
 def save_and_send_pdf(fig, chat_id, bot):
-   
+    """
+    save_and_send_pdf(fig, chat_id, bot): This function saves the Matplotlib figure as a PDF and sends it as a document via Telegram.
+
+    Args:
+        fig (Matplotlib.figure.Figure): Matplotlib figure object.
+        chat_id (int): Telegram chat ID.
+        bot (obj): The Telegram bot object.
+
+    Returns:
+        None
+    """
     pdf_path = "expense_history.pdf"
     fig.savefig(pdf_path)
     plt.close(fig)
@@ -45,7 +64,21 @@ def save_and_send_pdf(fig, chat_id, bot):
     bot.send_document(chat_id, open(pdf_path, "rb"))
 
 def run(message, bot):
-    
+    """
+    run(message, bot): This is the main function used to implement the pdf save feature.
+
+    Args:
+        message (obj): The message object received from Telegram.
+        bot (obj): The Telegram bot object.
+
+    Returns:
+        None
+
+    Raises:
+        Exception: If an error occurs during the process.
+
+    This function generates a PDF file containing the user's expense history plot and sends it as a document using a Telegram bot.
+    """
     try:
         helper.read_json()
         chat_id = message.chat.id
