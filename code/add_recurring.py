f36c116bcdbc8cb50b429b4efceea9e6d12b160a
Rishabh Kala <145221222+r-kala@users.noreply.github.com>
2023-11-23 14:47:37 -0500

    Update add_recurring.py to incorporate currency conversion feature
    Added new functions and updated existing ones to incorporate the currency selection and conversion functionality for recurring expenses.

--- code/add_recurring.py
+++ code/add_recurring.py
@@ -3,10 +3,10 @@ import logging
 from telebot import types
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
-
+from forex_python.converter import CurrencyRates
 
 option = {}
-
+currencies = CurrencyRates(force_decimal = False)
 
 def run(message, bot):
     helper.read_json()
@@ -19,18 +19,23 @@ def run(message, bot):
     msg = bot.reply_to(message, 'Select Category', reply_markup=markup)
     bot.register_next_step_handler(msg, post_category_selection, bot)
 
-
 def post_category_selection(message, bot):
     try:
         chat_id = message.chat.id
         selected_category = message.text
         if selected_category not in helper.getSpendCategories():
             bot.send_message(chat_id, 'Invalid', reply_markup=types.ReplyKeyboardRemove())
-            raise Exception("Sorry I don't recognise this category \"{}\"!".format(selected_category))
+            raise Exception("Sorry I don't recognize this category \"{}\"!".format(selected_category))
 
         option[chat_id] = selected_category
-        message = bot.send_message(chat_id, 'How much did you spend on {}? \n(Enter numeric values only)'.format(str(option[chat_id])))
-        bot.register_next_step_handler(message, post_amount_input, bot, selected_category)
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.row_width = 2
+        print("Currencies:")
+        for c in helper.getCurrencies():
+            print("\t", c)
+            markup.add(c)
+        msg = bot.reply_to(message, 'Select Currency', reply_markup = markup)
+        bot.register_next_step_handler(msg, post_currency_selection, bot, selected_category)
     except Exception as e:
         logging.exception(str(e))
         bot.reply_to(message, 'Oh no! ' + str(e))
@@ -42,12 +47,35 @@ def post_category_selection(message, bot):
         bot.send_message(chat_id, 'Please select a menu option from below:')
         bot.send_message(chat_id, display_text)
 
+# Contains step to run after the currency is selected
+def post_currency_selection(message, bot, selected_category):
+    try:
+        chat_id = message.chat.id
+        selected_currency = message.text
+        if selected_currency not in helper.getCurrencies():
+            bot.send_message(chat_id, 'Invalid', reply_markup=types.ReplyKeyboardRemove())
+            raise Exception("Sorry I don't recognize this currency \"{}\"!".format(selected_currency))
 
-def post_amount_input(message, bot, selected_category):
+        message = bot.send_message(chat_id, 'How much did you spend on {}? \n(Enter numeric values only)'.format(str(option[chat_id])))
+        bot.register_next_step_handler(message, post_amount_input, bot, selected_category, selected_currency)
+    except Exception as e:
+        logging.exception(str(e))
+        bot.reply_to(message, 'Oh no! ' + str(e))
+        display_text = ""
+        commands = helper.getCommands()
+        for c in commands:  # generate help text out of the commands dictionary defined at the top
+            display_text += "/" + c + ": "
+            display_text += commands[c] + "\n"
+        bot.send_message(chat_id, 'Please select a menu option from below:')
+        bot.send_message(chat_id, display_text)
+
+def post_amount_input(message, bot, selected_category, selected_currency):
     try:
         chat_id = message.chat.id
         amount_entered = message.text
         amount_value = helper.validate_entered_amount(amount_entered)  # validate
+        amount_value = currencies.convert(selected_currency,'USD',float(amount_value))
+        amount_value = str(round(float(amount_value), 2))
         if amount_value == 0:  # cannot be $0 spending
             raise Exception("Spent amount has to be a non-zero number.")
 
