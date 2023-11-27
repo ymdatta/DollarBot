7ae8a09b6f8bd2c1760fa6451c862a1a73007d4d
Rishabh Kala <145221222+r-kala@users.noreply.github.com>
2023-11-27 03:47:31 -0500

    Update test_budget_update_rest.py
    To accommodate the newly added functionalities, the test cases were updated accordingly.

--- test/test_budget_update_rest.py
+++ test/test_budget_update_rest.py
@@ -1,5 +1,6 @@
 from code import budget_update
 from mock import ANY
+from mock import Mock
 from mock.mock import patch
 from telebot import types
 
@@ -10,11 +11,12 @@ def test_update_overall_budget_already_available_case(mock_telebot, mocker):
     mc.send_message.return_value = True
 
     mocker.patch.object(budget_update, 'helper')
-    budget_update.helper.isOverallBudgetAvailable.return_value = True
-    budget_update.helper.getOverallBudget.return_value = 100
+    budget_update.helper.getCurrencies.return_value = ['USD', 'EUR', 'INR']
 
-    budget_update.update_overall_budget(120, mc)
-    mc.send_message.assert_called_with(120, ANY)
+    message = Mock()
+    message.text = 'USD'
+    budget_update.update_overall_budget(message, mc)
+    mc.reply_to.assert_called_with(message, 'Select Currency', reply_markup=ANY)
 
 
 @patch('telebot.telebot')
@@ -23,10 +25,13 @@ def test_update_overall_budget_new_budget_case(mock_telebot, mocker):
     mc.send_message.return_value = True
 
     mocker.patch.object(budget_update, 'helper')
+    budget_update.helper.getCurrencies.return_value = ['USD', 'EUR', 'INR']
     budget_update.helper.isOverallBudgetAvailable.return_value = True
 
-    budget_update.update_overall_budget(120, mc)
-    mc.send_message.assert_called_with(120, ANY)
+    message = Mock()
+    message.text = 'USD'
+    budget_update.update_overall_budget(message, mc)
+    mc.reply_to.assert_called_with(message, 'Select Currency', reply_markup=ANY)
 
 
 @patch('telebot.telebot')
@@ -39,7 +44,7 @@ def test_post_overall_amount_input_working(mock_telebot, mocker):
     budget_update.helper.validate_entered_amount.return_value = 150
 
     message = create_message("hello from testing")
-    budget_update.post_overall_amount_input(message, mc)
+    budget_update.post_overall_amount_input(message, mc, 'INR')
 
     mc.send_message.assert_called_with(11, ANY)
 
@@ -50,20 +55,20 @@ def test_post_overall_amount_input_nonworking(mock_telebot, mocker):
     mc.send_message.return_value = True
 
     mocker.patch.object(budget_update, 'helper')
+    budget_update.helper.getCurrencies.return_value = ['USD', 'EUR', 'INR']
     budget_update.helper.isOverallBudgetAvailable.return_value = True
-    budget_update.helper.validate_entered_amount.return_value = 0
-    budget_update.helper.throw_exception.return_value = True
 
-    message = create_message("hello from testing")
-    budget_update.post_overall_amount_input(message, mc)
-
-    assert (budget_update.helper.throw_exception.called)
+    message = Mock()
+    message.text = 'USD'
+    budget_update.update_overall_budget(message, mc)
+    mc.reply_to.assert_called_with(message, 'Select Currency', reply_markup=ANY)
 
 
 @patch('telebot.telebot')
 def test_update_category_budget(mock_telebot, mocker):
     mc = mock_telebot.return_value
-    mc.reply_to.return_value = True
+    reply_message = create_message("Food")
+    mc.reply_to.return_value = reply_message
 
     mocker.patch.object(budget_update, 'helper')
     budget_update.helper.getSpendCategories.return_value = ['Food', 'Groceries', 'Utilities', 'Transport', 'Shopping', 'Miscellaneous']
@@ -93,33 +98,34 @@ def test_post_category_selection_category_not_found(mock_telebot, mocker):
 @patch('telebot.telebot')
 def test_post_category_selection_category_wise_case(mock_telebot, mocker):
     mc = mock_telebot.return_value
-    mc.send_message.return_value = True
+    reply_message = create_message("Food")
+    mc.reply_to.return_value = reply_message
 
     mocker.patch.object(budget_update, 'helper')
     budget_update.helper.getSpendCategories.return_value = ['Food', 'Groceries', 'Utilities', 'Transport', 'Shopping', 'Miscellaneous']
-    budget_update.helper.getCategoryBudgetByCategory.return_value = 10
-    budget_update.helper.isCategoryBudgetByCategoryAvailable.return_value = True
+    budget_update.helper.getCurrencies.return_value = ['USD', 'EUR', 'GBP', 'INR', 'JPY']
 
     message = create_message("Food")
     budget_update.post_category_selection(message, mc)
 
-    mc.send_message.assert_called_with(11, ANY)
-    assert (budget_update.helper.getCategoryBudgetByCategory.called)
+    mc.reply_to.assert_called_with(message, 'Select Currency', reply_markup=ANY)
 
 
 @patch('telebot.telebot')
 def test_post_category_selection_overall_case(mock_telebot, mocker):
     mc = mock_telebot.return_value
-    mc.send_message.return_value = True
+    reply_message = create_message("Food")
+    mc.reply_to.return_value = reply_message
 
     mocker.patch.object(budget_update, 'helper')
     budget_update.helper.getSpendCategories.return_value = ['Food', 'Groceries', 'Utilities', 'Transport', 'Shopping', 'Miscellaneous']
-    budget_update.helper.isCategoryBudgetByCategoryAvailable.return_value = False
+    budget_update.helper.getCurrencies.return_value = ['USD', 'EUR', 'GBP', 'INR', 'JPY']
 
     message = create_message("Food")
     budget_update.post_category_selection(message, mc)
 
-    mc.send_message.assert_called_with(11, 'Enter monthly budget for Food\n(Enter numeric values only)')
+
+    mc.reply_to.assert_called_with(message, 'Select Currency', reply_markup=ANY)
 
 
 @patch('telebot.telebot')
@@ -131,7 +137,7 @@ def test_post_category_amount_input_working(mock_telebot, mocker):
     budget_update.helper.validate_entered_amount.return_value = 100
 
     message = create_message("Hello from testing")
-    budget_update.post_category_amount_input(message, mc, "Food")
+    budget_update.post_category_amount_input(message, mc, "Food", 'INR')
 
     mc.send_message.assert_called_with(11, 'Budget for Food Created!')
 
@@ -139,16 +145,22 @@ def test_post_category_amount_input_working(mock_telebot, mocker):
 @patch('telebot.telebot')
 def test_post_category_amount_input_nonworking_case(mock_telebot, mocker):
     mc = mock_telebot.return_value
-    mc.send_message.return_value = True
+    reply_message = create_message("100")
+    mc.send_message.return_value = reply_message
 
     mocker.patch.object(budget_update, 'helper')
-    budget_update.helper.validate_entered_amount.return_value = 0
-    budget_update.helper.throw_exception.return_value = True
+    mocker.patch.object(budget_update, 'currencies')
+    budget_update.helper.validate_entered_amount.return_value = 100
+    budget_update.currencies.convert.return_value = 73.5
+    budget_update.helper.read_json.return_value = {}
+    budget_update.helper.createNewUserRecord.return_value = {'budget': {'category': None}}
+    budget_update.helper.write_json.return_value = True
 
-    message = create_message("Hello from testing")
-    budget_update.post_category_amount_input(message, mc, "Food")
+    message = create_message("Food")
+    budget_update.post_category_amount_input(message, mc, "Food", "INR")
 
-    assert (budget_update.helper.throw_exception.called)
+    mc.send_message.assert_called_with(11, 'Budget for Food Created!')
+    assert (budget_update.helper.write_json.called)
 
 
 @patch('telebot.telebot')
