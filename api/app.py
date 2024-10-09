5b0ebbfe59d8ea8e3a88ec7f4a11aee67ea924b6
abhira0 <82208485+abhira0@users.noreply.github.com>
2024-10-09 06:37:54 +0000

    close mongoclient

--- api/app.py
+++ api/app.py
@@ -10,5 +10,12 @@ app.include_router(expenses.router)
 app.include_router(users.router)
 # app.include_router(budgets.router)
 
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    # Call the shutdown function for MongoDB client
+    users.shutdown_db_client()
+
+
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
