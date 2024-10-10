2bdb2853ec8ac45b383c274704c2d4b026a1ca5e
abhira0 <82208485+abhira0@users.noreply.github.com>
2024-10-10 08:03:52 +0000

    pylint app.py

--- api/app.py
+++ api/app.py
@@ -1,5 +1,6 @@
-# app.py
-import asyncio
+"""
+This module defines the main FastAPI application for Money Manager.
+"""
 
 import uvicorn
 from fastapi import FastAPI
@@ -8,14 +9,16 @@ from api.routers import expenses, users
 
 app = FastAPI()
 
-# # Include routers for different functionalities
+# Include routers for different functionalities
 app.include_router(expenses.router)
 app.include_router(users.router)
-# app.include_router(budgets.router)
 
 
 @app.on_event("shutdown")
 async def shutdown_event():
+    """
+    Handles the shutdown event to close the MongoDB client.
+    """
     # Call the shutdown function for MongoDB client
     await users.shutdown_db_client()
 
