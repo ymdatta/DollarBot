8963c3daa3e2611e124b48e3cbec39e9c4c9db9a
abhira0 <82208485+abhira0@users.noreply.github.com>
2024-10-23 01:52:06 +0000

    reord of routers

--- api/app.py
+++ api/app.py
@@ -2,23 +2,28 @@
 This module defines the main FastAPI application for Money Manager.
 """
 
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
-from api.routers import expenses, users
+
+from api.routers import accounts, expenses, users
+
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
-    # You can add startup logic here if needed
+async def lifespan():
+    """Lifespan"""
     yield
     # Handles the shutdown event to close the MongoDB client
     await users.shutdown_db_client()
 
+
 app = FastAPI(lifespan=lifespan)
 
 # Include routers for different functionalities
-app.include_router(expenses.router)
 app.include_router(users.router)
+app.include_router(accounts.router)
+app.include_router(expenses.router)
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
