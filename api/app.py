0ebce7fa8bb39c400f26843823f08e2524793e5a
AsthaBhalodiya <63735761+AsthaBhalodiya@users.noreply.github.com>
2024-10-31 20:34:43 +0000

    delete unnecessary files

--- api/app.py
+++ api/app.py
@@ -8,7 +8,7 @@ import uvicorn
 from fastapi import FastAPI
 
 from api.config import API_BIND_HOST, API_BIND_PORT
-from api.routers import accounts, categories, expenses, users
+from api.routers import accounts, analytics, categories, expenses, users
 
 
 @asynccontextmanager
@@ -26,6 +26,7 @@ app.include_router(users.router)
 app.include_router(accounts.router)
 app.include_router(categories.router)
 app.include_router(expenses.router)
+app.include_router(analytics.router)
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host=API_BIND_HOST, port=API_BIND_PORT, reload=True)
