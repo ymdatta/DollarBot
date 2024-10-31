e3b00756498d6b4a2a02858ba0741851eb85bdcb
abhira0 <82208485+abhira0@users.noreply.github.com>
2024-10-31 05:00:00 +0000

    changes

--- config.py
+++ config.py
@@ -4,17 +4,13 @@ This module contains configuration settings for the Money Manager application.
 
 import os
 
-MONGO_URI = os.getenv(
-    "MONGO_URI",
-    "mongodb+srv://mmdb_admin:tiaNSKxzyO2NdXts@moneymanagerdb.s2bp9.mongodb.net/"
-    "?retryWrites=true&w=majority&appName=MoneyManagerDB",
-)
+MONGO_URI = os.getenv("MONGO_URI", None)
 
-TOKEN_SECRET_KEY = "your_secret_key"
-TOKEN_ALGORITHM = "HS256"
+TOKEN_SECRET_KEY = os.getenv("TOKEN_SECRET_KEY", None)
+TOKEN_ALGORITHM = os.getenv("TOKEN_ALGORITHM", None)
 
-API_BIND_HOST = "0.0.0.0"
-API_BIND_PORT = 9999
+API_BIND_HOST = os.getenv("API_BIND_HOST", "0.0.0.0")
+API_BIND_PORT = os.getenv("API_BIND_PORT", 9999)
 
-TELEGRAM_BOT_TOKEN = "7601886777:AAEqIw5gVUPBuDqVeWwR1GdG_Y8eiA0JKFA"
-TELEGRAM_BOT_API_BASE_URL = "http://localhost:9999"
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", None)
+TELEGRAM_BOT_API_BASE_URL = os.getenv("TELEGRAM_BOT_API_BASE_URL", "http://localhost:9999")
